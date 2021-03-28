11b3beb89325abcae1bf52cb97938492c61aa722
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-27 21:41:33 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+// Static Class Attributes | C# | Tutorial 29
+
+namespace CSharpExercises_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this calls the "Song"contructor within the class
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(holiday.title);
+            Console.WriteLine(Song.songCount);
+
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.title);
+            //Console.WriteLine(Song.songCount);
+
+            // this method returns the songcount on each object if you want
+            // using a getter method we put inside the class that returns songCount 
+            // vs using the static class attribute above
+
+            Console.WriteLine(kashmir.getSongCount());
+
+            Console.WriteLine(holiday.artist);
+            Console.WriteLine(kashmir.artist);
+          
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExercises_29
+{
+    class Song
+    {
+        // static attributes in classes in C#
+        // special type of attribute in a class that is shared by ALL objects AND 
+        // ALL instances of that class
+        // AKA an attribute that is on the class vs being on the object of the class
+
+        public string title;
+        public string artist;
+        public int duration;
+        // create static variable and we set to 0
+        public static int songCount = 0;
+
+        // this is a contructor to create a new song object
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            // each time song object is created we increment the count
+            songCount++;
+        }
+
+        // this getter method returns the songcount on each object if you want
+        public int getSongCount()
+        {
+            return songCount;
+        }
+
+    }
+}
